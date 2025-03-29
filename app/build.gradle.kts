c03a9af5c8246e9337b16106c7a082c48d52d73d
Daria Deriagina <w0862447@myscc.ca>
2025-03-29 08:47:59 -0400

    fixed dependences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,7 @@ android {
 }
 
 dependencies {
-    implementation(libs.gson)
-
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
