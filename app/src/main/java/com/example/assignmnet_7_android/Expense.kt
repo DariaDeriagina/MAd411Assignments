32e0e3fc232425ba0ebc8770720ec5c1883155dc
Daria Deriagina <w0862447@myscc.ca>
2025-03-29 18:57:55 -0400

    Updated Expense data class with currency and convertedCost

--- app/src/main/java/com/example/assignmnet_7_android/Expense.kt
+++ app/src/main/java/com/example/assignmnet_7_android/Expense.kt
@@ -1,7 +1,13 @@
 package com.example.assignmnet_7_android
 
+import java.util.Currency
+
 data class Expense(
     val name: String,
     val amount: Double,
-    val date: String
+    val date: String,
+    val costAssociated: Boolean,
+    val currency: Currency,
+    val convertedCost: Double
 )
+
